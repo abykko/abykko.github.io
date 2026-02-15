b7b507b41595359dce3faa345c7080fc94b7bd22
abykko <ismaelah102@gmail.com>
2026-02-15 21:47:51 +0100

    Cambios de responsividad, aun queda por cubrir muchos aspectos.

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,31 @@ import TechnologiesAndEducation from "./components/TechnologiesAndEducation/main
 
 export function CvContainer({ children }: { children: ReactNode }) {
   return (
-    <div className="w-1/1 h-1/1 pt-10 max-h-screen overflow-scroll">
+    <div className="
+        w-1/1
+        h-1/1
+        max-h-screen
+        overflow-scroll
+        xl:pt-10
+    ">
       <div
         className="
-          flex flex-col items-center justify-center
-          w-4/6 px-20 py-15 mx-auto rounded-lg shadow-xl
-          bg-gray-100/10
+          flex 
+          flex-col
+          items-center
+          justify-center
           backdrop-blur-lg
           backdrop-brightness-80
           backdrop-contrast-130
           backdrop-saturate-110
+          xl:w-5/6
+          2xl:w-5/7
+          xl:rounded-lg
+          xl:shadow-xl
+          px-20
+          py-15
+          mx-auto
+          bg-gray-100/10
         "
       >
         {children}
